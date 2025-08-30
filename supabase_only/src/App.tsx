4532ea3fdcfb0b1a8cc31ca7bf4d58edefa59140
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-08-31 01:50:22 +0600

    use effect on auth state change on the page and unsubscibe the listener

--- supabase_only/src/App.tsx
+++ supabase_only/src/App.tsx
@@ -3,16 +3,17 @@ import './App.css'
 import Sign from './components/auth';
 import TaskManager from './components/task-manager';
 import { supabase } from './assets/supabase-client';
+import type { Session } from '@supabase/supabase-js';
 
 function App() {
 
-  const [session, setSession] = useState(null);
+  const [session, setSession] = useState<Session | null>(null);
 
   const fetchSession = async () => {
     const { data: currentSession } = await supabase.auth.getSession();
 
-    setSession(currentSession);
-    console.log(currentSession);
+    setSession(currentSession.session);
+    console.log(currentSession.session);
     
   }
 
@@ -24,11 +25,19 @@ function App() {
       return;
     }
 
-    setSession(null);
+    fetchSession();
   }
 
   useEffect(() => {
     fetchSession();
+
+    const {data} = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => {
+      data.subscription.unsubscribe();
+    }
   }, [])
 
   return (
