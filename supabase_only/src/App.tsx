71b35a8ffbd327a6a575b35a360e1e959ef2cb94
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-08-31 01:20:42 +0600

    authenticate the user by session

--- supabase_only/src/App.tsx
+++ supabase_only/src/App.tsx
@@ -1,15 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import Sign from './components/auth';
 import TaskManager from './components/task-manager';
+import { supabase } from './assets/supabase-client';
 
 function App() {
 
+  const [session, setSession] = useState(null);
+
+  const fetchSession = async () => {
+    const currentSession = await supabase.auth.getSession();
+
+    setSession(currentSession.data);
+    console.log(currentSession);
+    
+  }
+
+  useEffect(() => {
+    fetchSession();
+  }, [])
+
   return (
     <>
-      <TaskManager />
-
-      {/* sign in/ sign out section */}
-      <Sign />
+      {session ? (
+        <button>Logout</button>
+        <TaskManager /> 
+      ) : <Sign />}
     </>
   )
 }
