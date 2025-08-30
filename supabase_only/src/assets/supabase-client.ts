91435aa4a263cefcbd8f0b24e85b894484c6ca40
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-08-30 22:23:23 +0600

    basic ui for task manager

--- supabase_only/src/assets/supabase-client.ts
+++ supabase_only/src/assets/supabase-client.ts
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // but keep these in .local.env file to avoid leaking secrets
 const supabaseUrl = 'https://kdlfnnthuvqwdyelcudt.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkbGZubnRodXZxd2R5ZWxjdWR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1NDA5ODUsImV4cCI6MjA3MjExNjk4NX0.OXXXgk8Is2_O7ClukFNuctPoHAnuZVDVZjAkVPox-94'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkbGZubnRodXZxd2R5ZWxjdWR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1NDA5ODUsImV4cCI6MjA3MjExNjk4NX0.OXXXgk8Is2_O7ClukFNuctPoHAnuZVDVZjAkVPox-94"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
